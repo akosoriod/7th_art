d15e827e095ee879d063c67c000f0b61d4e30525
maparrar <maparrar@gmail.com>
2014-04-08 02:07:20 -0500

    Experimentos con el editor

---
+++ experiments/editor/editor.php
@@ -0,0 +1,41 @@
+<html>
+    <head>
+        <title>Editor experiment</title>
+        <link rel="stylesheet" type="text/css" href="../../css/reset.css">
+        <link rel="stylesheet" type="text/css" href="editor.css">
+        
+        
+        <link rel="stylesheet" type="text/css" href="../jquery/jquery-ui.min.css">
+        <script src="../jquery/jquery-2.1.0.min.js"></script>
+        <script src="../jquery/jquery-ui.min.js"></script>
+        <script type="text/javascript">
+            $( document ).ready(function(){
+                $("#editor").click(function(){
+                    alert("Hi 7th @rt");
+                });
+            });
+        </script>
+    </head>
+    <body>
+        <header>
+            <div id="header_7th_art">7th @rt administrator</div>
+        </header>
+        <div id="container">
+            <nav id="navigation">
+                sections
+            </nav>
+            <div id="workspace">
+                <div id="editor">
+                    <div id="toolbar">
+                        <div class="object">image</div>
+                        <div class="object">text</div>
+                        <div class="object">text / image</div>
+                    </div>
+                    <div>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
