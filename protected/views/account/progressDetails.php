37bbdaa5427b3baaf58579217d0aa3340917f4ba
oscarparuma <oscar.paruma@gmail.com>
2014-11-17 11:10:55 -0500

    Estructura HTML y estilos para Film progress details y Top Ranking. Se habilitó la autenticación independiente para usuarios finales y para usuarios con rol administrator y operator.

---
+++ protected/views/account/progressDetails.php
@@ -0,0 +1,151 @@
+
+    <div class="Table">
+        <div class="Title">
+            <div class="TitleCell">Shine</div>
+            <div class="TitleCell"></div>
+            <div class="TitleCell"></div>
+            <div class="TitleCell">60%</div>
+        </div>
+        <div>&nbsp;</div>
+        <div class="Heading">
+            <div class="Cell">
+                <p>ACTIVITY</p>
+            </div>
+            <div class="Cell">
+                <p>PERCENTAGE</p>
+            </div>
+        </div>
+        <div class="Row">
+            <div class="Cell">
+                <p>Synopsis</p>
+            </div>
+            <div class="Cell">
+                <p>8%</p>
+            </div>
+        </div>
+        <div class="Row">
+            <div class="caption">
+                <p>Pre-Viewing</p>
+            </div>
+            <div class="Row">
+                <div class="Cell NestedCell">
+                    <p>1</p>
+                </div>
+                <div class="Cell NestedCell">
+                    <p>7%</p>
+                </div>
+            </div>
+            <div class="Row">
+                <div class="Cell NestedCell">
+                    <p>2</p>
+                </div>
+                <div class="Cell NestedCell">
+                    <p>7%</p>
+                </div>
+            </div>
+            <div class="Cell">
+                <p>14%</p>
+            </div>
+        </div>
+        <div class="Row">
+            <div class="Cell">
+                <p>Who's who in ...?</p>
+            </div>
+            <div class="Cell">
+                <p>4%</p>
+            </div>
+        </div>
+        <div class="Row">
+            <div class="caption">
+                <p>Film-Based</p>
+            </div>
+            <div class="Row">
+                <div class="Cell NestedCell">
+                    <p>1</p>
+                </div>
+                <div class="Cell NestedCell">
+                    <p>0%</p>
+                </div>
+            </div>
+            <div class="Row">
+                <div class="Cell NestedCell">
+                    <p>2</p>
+                </div>
+                <div class="Cell NestedCell">
+                    <p>0%</p>
+                </div>
+            </div>
+            <div class="Cell">
+                <p>0%</p>
+            </div>
+        </div>
+        <div class="Row">
+            <div class="Cell">
+                <p>Spidermap</p>
+            </div>
+            <div class="Cell">
+                <p>6%</p>
+            </div>
+        </div>
+        <div class="Row">
+            <div class="caption">
+                <p>After-Viewing</p>
+            </div>
+            <div class="Row">
+                <div class="Cell NestedCell">
+                    <p>1</p>
+                </div>
+                <div class="Cell NestedCell">
+                    <p>7%</p>
+                </div>
+            </div>
+            <div class="Row">
+                <div class="Cell NestedCell">
+                    <p>2</p>
+                </div>
+                <div class="Cell NestedCell">
+                    <p>7%</p>
+                </div>
+            </div>
+            <div class="Cell">
+                <p>14%</p>
+            </div>
+        </div>
+        <div class="Row">
+            <div class="Cell">
+                <p>Spidermap</p>
+            </div>
+            <div class="Cell">
+                <p>6%</p>
+            </div>
+        </div>
+        <div class="Row">
+            <div class="Cell">
+                <p>The Expert Says ...</p>
+            </div>
+            <div class="Cell">
+                <p>10%</p>
+            </div>
+        </div>
+        <div class="Row">
+            <div class="Cell">
+                <p>Did you know that ...?</p>
+            </div>
+            <div class="Cell">
+                <p>4%</p>
+            </div>
+        </div>
+        <div class="Row">
+            <div class="Cell">
+                <p>TOTAL</p>
+            </div>
+            <div class="Cell">
+                <p>60%</p>
+            </div>
+            <div class="Right">
+            <?php 
+            print CHtml::image(Yii::app()->request->baseUrl."/images/account/incompleteCamera.png","","");
+            ?>
+            </div>
+        </div>
+    </div>
