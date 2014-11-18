508eaba165f57a2b446bdbf395468d440746bb93
7th-art-dev <sistemas.7th.art@gmail.com>
2014-11-18 17:41:59 -0500

    Version de operadores corregida mostrando sets de actividades asignados y finalizados por el operador en sesión.

---
+++ protected/views/activitySet/_oper_view.php
@@ -0,0 +1,48 @@
+<?php
+/* @var $this ActivitySetController */
+/* @var $data ActivitySet */
+?>
+
+<div class="activitySet">
+	<b><?php echo CHtml::encode($data->getAttributeLabel('title')); ?>:</b>
+	<?php echo CHtml::encode($data->title); ?>
+	<br />
+        
+        <b><?php echo CHtml::encode($data->getAttributeLabel('director')); ?>:</b>
+	<?php echo CHtml::encode($data->director); ?>
+	<br />
+        
+        <b><?php echo CHtml::encode($data->getAttributeLabel('year')); ?>:</b>
+	<?php echo CHtml::encode($data->year); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('status_id')); ?>:</b>
+	<?php echo $data->statusSpanish(); ?>
+	<br />
+
+	<!--<b><?php // echo CHtml::encode($data->getAttributeLabel('publication')); ?>:</b>-->
+	<?php // echo CHtml::encode($data->publication); ?>
+	<!--<br />-->
+
+	<!--<b><?php // echo CHtml::encode($data->getAttributeLabel('tagline')); ?>:</b>-->
+	<?php // echo CHtml::encode($data->tagline); ?>
+	<!--<br />-->
+
+	<?php /*
+	<b><?php echo CHtml::encode($data->getAttributeLabel('soundtrack_id')); ?>:</b>
+	<?php echo CHtml::encode($data->soundtrack_id); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('image_id')); ?>:</b>
+	<?php echo CHtml::encode($data->image_id); ?>
+	<br />
+        */ ?>
+        
+	<?php /* echo CHtml::encode($data->getAttributeLabel('operator_id')); ?>:</b>
+	<?php echo CHtml::encode($data->operator->name." ".$data->operator->lastname); */ ?>
+        
+	<?php echo CHtml::link("Editar", array('designer/index')); ?>
+	<!-- <?php echo CHtml::link("Editar Set de Actividades", array('edit')); ?> -->
+	<br />
+
+</div>
---
+++ protected/views/activitySet/oper.php
@@ -0,0 +1,94 @@
+<?php
+/* @var $this ActivitySetController */
+/* @var $activitySets ActivitySet */
+/* @var $currentUser User */
+/* @var $users User */
+$this->breadcrumbs=array(
+	'Operador',
+);
+
+Yii::app()->clientScript->registerCssFile(Yii::app()->baseUrl.'/css/administrator.css');
+?>
+<script type="text/javascript">
+    $( document ).ready(function(){
+//        $(".site-url").empty().append('<div class="icon"> </div> 7 <sup>th</sup> @rt Administator');
+        var appUrl="<?php // echo Yii::app()->baseUrl."/"; ?>";
+//        var editor=new Editor({
+//            appUrl:appUrl
+//        });
+//        editor.init();
+//
+//        var sections=$("#navigation").children("ul");
+//        sections.click(function(){
+//            alert("only a prototype");
+//        });
+    });
+</script>
+<main id="oper_page" class="oper_page">
+    <?php
+    $this->breadcrumbs=array(
+	'Activity Sets',
+    );
+    ?>
+    <div id="container">
+        <div id="activitySets" class="section">
+            <h3 class="section_title">Sets de actividades asignados</h3>
+            
+            <div class="list">
+			        <?php
+				    foreach ($activitySets as $activitySet) {
+					// Selecciona unicamente los sets de actividades asignados al operador
+					if($activitySet->operator_id == $currentUser->id){
+						// Que estén en edición
+						if($activitySet->status_id == 1){					
+							$this->renderPartial('_oper_view',array('data'=>$activitySet));
+						}
+					}
+				    }
+				?>                
+
+            </div>
+	    <!--
+	    <div align="center" class="row row4">
+	    <table style="width:30%">
+		  <tr>
+		    <td>Select activity set to edit:</td>
+		    <td>
+			<select>
+			  	<?php
+				    foreach ($activitySets as $activitySet) {
+					if($activitySet->operator_id == $currentUser->id){					
+						?>
+						<option><?php echo CHtml::encode($activitySet->title); ?> </option>
+						<?php
+					}
+				    }
+				?>
+			</select> 
+		    </td>
+		    <td> <button type="button">Edit</button> </td>
+		  </tr>
+		  <tr height="30px">
+		  </tr>
+	     </table> 
+	</div> -->
+        
+    </div>
+
+	<div id="completedActivitySets" class="section">
+            <h3 class="section_title">Sets de actividades aprobados</h3>
+            
+            <div class="list">
+                		<?php
+				    foreach ($activitySets as $activitySet) {
+					if($activitySet->operator_id == $currentUser->id){
+						if($activitySet->status_id != 1){					
+							$this->renderPartial('_oper_view',array('data'=>$activitySet));
+						}
+					}
+				    }
+				?>   
+            </div>
+
+	    
+</main>
