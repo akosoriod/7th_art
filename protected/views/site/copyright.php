f9a9db286efe36c50c25f14b58b7dc9f72b9b3ba
oscarparuma <oscar.paruma@gmail.com>
2015-03-15 21:36:22 -0500

    Términos de uso, de acuerdo al archivo Términos de Uso.docx. About Us (English version), de acuerdo al archivo About Us - English.docx. Página legal, de acuerdo al archivo Página legal.docx

---
+++ protected/views/site/copyright.php
@@ -0,0 +1,175 @@
+        <main class="detalle">
+            <div class="row row1">
+                <div id="divAboutUs" class="col-xs-12 col-sm-12 col-md-7">
+                    <h2>7<sup>th</sup> @rt: The Power &amp; Magic of Films to Learn English</h2>
+                    <p>
+                    <b>UNIVERSIDAD NACIONAL DE COLOMBIA</b>
+                    <br/>
+                    <b>VICERRECTORÍA ACADÉMICA</b>
+                    <br/>
+                    <b>DIRECCIÓN ACADÉMICA SEDE BOGOTÁ</b>
+                    </p>
+                    <p>
+                    <b>Rector</b>	Ignacio Mantilla Prada
+                    </p>
+                    <p>
+                    <b>Vice-Rector</b>	Diego Hernández Losada
+                    </p>
+                    <p>
+                    <b>Vice-Rector Académico, Sede Bogotá</b>	Juan Manuel Tejeiro
+                    </p>
+                    <p>
+                    <b>Director Académico, Sede Bogotá</b>	Gerardo Rodríguez Niño
+                    </p>
+                    <p>
+                    <b>Vice-Rectora de Investigación</b>	Dolly Montoya Castaño
+                    </p>
+                    <p>
+                    <b>Dirección de Investigación, Sede Bogotá</b>	Luis Fernando Niño
+                    </p>
+                    <p>
+                    <b>Producción</b>	Dirección Académica, Sede Bogotá
+                    </p>
+                    <p>
+                    <b>Investigadora</b>	Elizabeth Rodríguez Reyes
+                    </p>
+                    <p>
+                    <b>Auxiliar de investigación</b>	Rafael Vera
+                    </p>
+                    <p>
+                    <b>Creadora</b>	Elizabeth Rodríguez Reyes
+                    </p>
+                    <p>
+                    <b>Formato de actividades de aprendizaje</b>	Elizabeth Rodríguez
+                    </p>
+                    <p>
+                    <b>Productora</b>	Elizabeth Rodríguez Reyes
+                    </p>
+                    <p>
+                    <b>Auxiliares de producción</b>	Alejandro Angarita
+                    <br/>
+                        Diego Cortés Ruíz
+                    </p>
+                    <p>
+                    <b>Desarrollo de Contenido</b>	Elizabeth Rodríguez Reyes
+                    </p>
+                    <p>
+                    <b>Corrección de estilo pre-virtualización</b>	Paul Priolet
+                    </p>
+                    <p>
+                    <b>Corrección de estilo pos-virtualización</b>	Paul Priolet
+                    <br/>
+                        Elizabeth Rodríguez Reyes
+                    </p>
+                    <p>
+                    <b>Desarrollo Plataforma Web</b>	Jean Pierre Charalambos
+                    <br/>
+                        Miguel Alejandro Parra
+                    <br/>
+                        Óscar Paruma Pabón
+                    </p>
+                    <p>
+                    <b>Auxiliar de desarrollo plataforma web</b>	Esteban Parra Rodríguez
+                    <br/>
+                        Grace Cabana Hernández
+                    </p>
+                    <p>
+                    <b>Diseño de interfaz web</b>	Elizabeth Rodríguez Reyes
+                    </p>
+                    <p>
+                    <b>Dirección de arte</b>	Elizabeth Rodríguez Reyes
+                    </p>
+                    <p>
+                    <b>Diagramación y Maquetación Web</b>	Javier Martínez Correa
+                    <br/>
+                        Mónica Contreras
+                    </p>
+                    <p>
+                    <b>Consultores</b>	Adrián Comas Mercado
+                    <br/>
+                        Farley Ortiz Forero
+                    <br/>
+                        Javier Martínez Correa
+                    <br/>
+                        Jorge Arturo Mora Espinosa
+                    <br/>
+                        Alberto Amaya Calderón
+                    <br/>
+                        Gabriel Alba
+                    <br/>
+                        Javier Olarte Triana
+                    <br/>
+                        José Ismael Peña
+                    <br/>
+                        Óscar Germán Duarte
+                    <br/>
+                        Jaime Mayorga Pinzón
+                    <br/>
+                        Juan de la Rosa
+                    </p>
+                    <p>
+                    <b>Pruebas de usabilidad</b>	
+                    </p>
+                    <p>
+                    <b>Con el apoyo de</b>
+                    <br/>
+                    Unidad de Programas Curriculares, Dirección Académica, Sede Bogotá
+                    <br/>
+                    Departamento de Lenguas Extranjeras
+                    <br/>
+                    Departamento de Ingeniería de Sistemas e Industrial
+                    <br/>
+                    Departamento de Comunicación Humana
+                    <br/>
+                    Escuela de Cine y Televisión
+                    <br/>
+                    Escuela de Diseño Gráfico
+                    <br/>
+                    Escuela de Diseño Industrial
+                    <br/>
+                    Conservatorio de Música
+                    <br/>
+                    Dirección Nacional de Bibliotecas
+                    <br/>
+                    Unimedios
+                    <br/>
+                    UN Radio
+                    <br/>
+                    Paradigma Web
+                    </p>
+                    <p>
+                    <b>Directora-Editora General</b>	Elizabeth Rodríguez Reyes
+                    </p>
+                    <p>
+                    <b>Auxiliares de dirección-edición</b>	Diego Cortés Ruíz
+                    <br/>
+                        Jhon Herrera Abril
+                    <br/>
+                        Giovanna Rodríguez Ortega
+                    <br/>
+                        Brayan Sáenz Beltrán
+                    <br/>
+                        Giovanny Vélez Beltrán
+                    </p>
+                    <p>
+                    <b>2015</b>
+                    </p>
+                    <p>
+                    &copy; Todos los derechos reservados. Queda  estrictamente prohibida cualquier forma total o parcial de reproducción, almacenamiento en un sistema de recuperación de datos, transmisión de cualquier forma electrónica o mecánica o grabación, sin el permiso previo y por escrito de la Universidad Nacional de Colombia. Esta herramienta y cualquier otro material con derechos reservados presentado en ella, es de uso estrictamente académico en
+                    </p>
+                    <p>
+                        <div id="div_img_logo_unal" class="col-xs-12 col-sm-12 col-md-5">
+                            <img id="img_logo_unal" src="<?php echo Yii::app()->request->baseUrl; ?>/images/test/logo_unal.png" height="100%" width="100%" alt="Escudo UNal"/>
+                        </div>
+                    </p>
+                    <p>
+                    <br/>
+                    <br/>
+                    <br/>
+                    VICERRECTORÍA DE SEDE
+                    <br>
+                    <b>DIRECCIÓN ACADÉMICA SEDE BOGOTÁ</b>
+                    </p>
+                </div>
+            </div>
+        </main>
--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -38,20 +38,80 @@ Yii::app()->clientScript->registerCssFile(Yii::app()->baseUrl.'/css/activity_set
     <div class="row row3">
         <div id="links" class="col-xs-12 col-sm-12 col-md-12">
             <div class="link">
+                <?php
+                $this->beginWidget('zii.widgets.jui.CJuiDialog', array(
+                    'id'=>'aboutus',
+                    'cssFile'=>Yii::app()->baseUrl.'/css/activity_sets.css',
+                    'options'=>array(
+                        'title'=>'ABOUT US (English version)',
+                        'width'=>760,
+                        'height'=>580,
+                        'autoOpen'=>false,
+                        'resizable' => false,
+                    ),
+                ));
+                require_once(Yii::app()->basePath . '/views/site/terms_of_use.php');
+                $this->endWidget('zii.widgets.jui.CJuiDialog');
+                ?>
                 <img src="<?php echo Yii::app()->request->baseUrl; ?>/images/test/ico_r2_c2.png" height="22" width="22" alt="" />
-                <a href="#">About Us</a>
+                <?php
+                print CHtml::link('About Us',
+                    array('controller/action','param1'=>'value1'),
+                    array('onclick'=>'$("#aboutus").dialog("open"); return false;')
+                );
+                ?>
             </div>
             <div class="link">
+                <?php
+                $this->beginWidget('zii.widgets.jui.CJuiDialog', array(
+                    'id'=>'copyright',
+                    'cssFile'=>Yii::app()->baseUrl.'/css/activity_sets.css',
+                    'options'=>array(
+                        'title'=>'Copyright',
+                        'width'=>740,
+                        'height'=>580,
+                        'autoOpen'=>false,
+                        'resizable' => false,
+                    ),
+                ));
+                require_once(Yii::app()->basePath . '/views/site/copyright.php');
+                $this->endWidget('zii.widgets.jui.CJuiDialog');
+                ?>
                 <img src="<?php echo Yii::app()->request->baseUrl; ?>/images/test/ico_r2_c4.png" height="22" width="22" alt="" />
-                <a href="#">Copyright page</a>
+                <?php
+                print CHtml::link('Copyright page',
+                    array('controller/action','param1'=>'value1'),
+                    array('onclick'=>'$("#copyright").dialog("open"); return false;')
+                );
+                ?>
             </div>
             <div class="link">
                 <img src="<?php echo Yii::app()->request->baseUrl; ?>/images/test/ico_r2_c6.png" height="22" width="22" alt="" />
                 <a href="#">Flash Version</a>
             </div>
             <div class="link">
+                <?php
+                $this->beginWidget('zii.widgets.jui.CJuiDialog', array(
+                    'id'=>'terms_of_use',
+                    'cssFile'=>Yii::app()->baseUrl.'/css/activity_sets.css',
+                    'options'=>array(
+                        'title'=>'T&eacute;rminos de Uso',
+                        'width'=>960,
+                        'height'=>580,
+                        'autoOpen'=>false,
+                        'resizable' => false,
+                    ),
+                ));
+                require_once(Yii::app()->basePath . '/views/site/about_us.php');
+                $this->endWidget('zii.widgets.jui.CJuiDialog');
+                ?>
                 <img src="<?php echo Yii::app()->request->baseUrl; ?>/images/test/ico_r2_c8.png" height="22" width="22" alt="" />
-                <a href="#">Terms of Use</a>
+                <?php
+                print CHtml::link('Terms of Use',
+                    array('controller/action','param1'=>'value1'),
+                    array('onclick'=>'$("#terms_of_use").dialog("open"); return false;')
+                );
+                ?>
             </div>
             <div class="link">
                 <img src="<?php echo Yii::app()->request->baseUrl; ?>/images/test/ico_r2_c10.png" height="22" width="22" alt="" />
